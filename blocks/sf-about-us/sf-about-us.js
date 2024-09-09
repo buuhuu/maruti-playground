b6d7749fa9affc1be4d6cf68a59e63d7746db0a0
Harsh Mehta <hmehta@adobe.com>
2024-09-09 12:17:03 +0530

    Collapse based on height

--- blocks/sf-about-us/sf-about-us.js
+++ blocks/sf-about-us/sf-about-us.js
@@ -1,12 +1,6 @@
 import utility from '../../utility/utility.js';
 
-const maxLength = 339;
-
 export default async function decorate(block) {
-  let fullText = '';
-  let truncatedText = '';
-  let displayText = '';
-
   function updateHtml() {
     const faqBlock = [...block.children].map((child) => {
       const [titleEl, subtitleEl, contentEl, toggleButtonEl] = [...child.children[0].children];
@@ -14,15 +8,7 @@ export default async function decorate(block) {
       const title = titleEl?.textContent?.trim();
       const subtitle = subtitleEl?.textContent?.trim();
       const toggleButton = toggleButtonEl?.textContent?.trim();
-
-      fullText = contentEl.innerHTML;
-      truncatedText = `${fullText.substring(0, maxLength)}...`;
-      if (document.querySelectorAll('.adobe-ue-edit .block.sf-about-us').length > 0) {
-        // In edit mode, show full text
-        displayText = fullText.replace(/<\/?p>/gi, '');
-      } else {
-        displayText = `${fullText.substring(0, maxLength)}...`.replace(/<\/?p>/gi, '');
-      }
+      const displayText = contentEl.innerHTML;
       child.innerHTML = '';
       child.insertAdjacentHTML(
         'beforeend',
@@ -45,12 +31,13 @@ export default async function decorate(block) {
     const contentEl = document.querySelector('.sf-text-content');
     const toggleButtonEl = document.querySelector('.toggle-read-button');
     toggleButtonEl.addEventListener('click', () => {
-      if (toggleButtonEl.textContent === 'Read More') {
-        contentEl.innerHTML = fullText;
-        toggleButtonEl.textContent = 'Read Less';
+
+      if (contentEl.classList.contains('expanded')) {
+        contentEl.classList.remove('expanded');
+        toggleButtonEl.textContent = 'Read more';
       } else {
-        contentEl.innerHTML = truncatedText;
-        toggleButtonEl.textContent = 'Read More';
+        contentEl.classList.add('expanded');
+        toggleButtonEl.textContent = 'Read less';
       }
     });
   }
