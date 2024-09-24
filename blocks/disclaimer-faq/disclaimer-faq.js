a3ad055e6456e084b79395894c498d8f4965df9a
Harsh Mehta <hmehta@adobe.com>
2024-09-24 12:37:00 +0530

    disclaimer faq

---
+++ blocks/disclaimer-faq/disclaimer-faq.js
@@ -0,0 +1,25 @@
+import { moveInstrumentation } from '../../commons/scripts/scripts.js';
+
+export default function decorate(block) {
+  const [titleContainer, ...faqItems] = block.children;
+  titleContainer.classList.add('faq-title');
+
+  const listItems = faqItems.map((item, index) => {
+    const [label, body] = item.children;
+    body.classList.add('faq-item-body');
+    const summary = document.createElement('summary');
+    summary.append(...label.childNodes);
+    summary.classList.add('faq-item-label');
+    const details = document.createElement('details');
+    details.append(summary, body);
+    details.classList.add('faq-item');
+    details.open = index === 0;
+    moveInstrumentation(item, details);
+    item.remove();
+    return details;
+  });
+  const list = document.createElement('div');
+  list.classList.add('faq-list');
+  list.append(...listItems);
+  block.replaceChildren(list);
+}
